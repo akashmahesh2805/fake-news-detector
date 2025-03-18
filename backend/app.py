47f595a5fc804c99193cdf16ebae89c98117f5de
Akash <akashmahesh2805@gmail.com>
2025-03-18 22:49:27 +0530

    Parallel processing in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import joblib
 
-# Load the trained model & vectorizer
+# Load model & vectorizer
 model = joblib.load("C:/PERSONAL/Coding/Projects/fake-news-detector/backend/model.pkl")
 vectorizer = joblib.load("C:/PERSONAL/Coding/Projects/fake-news-detector/backend/vectorizer.pkl")
 
@@ -10,8 +10,12 @@ app = FastAPI()
 @app.post("/predict")
 def predict(data: dict):
     text = data["title"]
-    text_vectorized = vectorizer.transform([text])
-    prediction = model.predict(text_vectorized)[0]
+    
+    # Speed up processing
+    with joblib.parallel_backend("threading"):
+        text_vectorized = vectorizer.transform([text])
+        prediction = model.predict(text_vectorized)[0]
+    
     return {"prediction": "FAKE" if prediction == 1 else "REAL"}
 
 if __name__ == "__main__":
